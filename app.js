c93f77e01d7106f77a0945b8b546351e0aa87d67
Matthew Randall <mattrandall01@gmail.com>
2021-01-21 18:31:13 -0500

    books routes relocated to their own file

--- app.js
+++ app.js
@@ -1,44 +1,17 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import Book from './models/bookModel.js'
+import bodyParser from 'body-parser';
+
+// Routes
+import bookRouter from './routes/bookrouter.js';
 
 const app = express();
 const db = mongoose.connect('mongodb://localhost/bookAPI')
-const bookRouter = express.Router();
 const port = process.env.PORT || 3000;
 
-bookRouter.route('/books')
-  .get((req, res) => {
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
-    // If something weird is typed into api call everything is returned.
-    // Does this make sense? feels better if nothing is returned if the user
-    // types in something weird...
-
-    // This is also case sensitive, fix that
-    const query = {};
-    if(req.query.genre) {
-      query.genre = req.query.genre;
-    }
-
-    Book.find(query, (err, books) => {
-      if(err){
-        return res.send(err);
-      }
-        
-      return res.json(books);     
-    });
-  });
-
-bookRouter.route('/books/:bookId')
-  .get((req, res) => {
-    Book.findById(req.params.bookId, (err, book) => {
-      if(err){
-        return res.send(err);
-      }
-        
-      return res.json(book);     
-    });
-  });
 app.use('/api', bookRouter);
 
 app.get('/', (req, res) => {
