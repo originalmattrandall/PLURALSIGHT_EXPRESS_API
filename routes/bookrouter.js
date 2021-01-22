3b1b6570738f2219d9c164864789a4fbdc84ae7a
Matthew Randall <mattrandall01@gmail.com>
2021-01-22 12:25:17 -0500

    adding delete call

--- routes/bookrouter.js
+++ routes/bookrouter.js
@@ -35,7 +35,7 @@ export default (Book) => {
         return next();
       }
 
-      return req.sendStatus(404);
+      return res.sendStatus(404);
     });
   });
 
@@ -49,9 +49,41 @@ export default (Book) => {
       book.genre = req.body.genre;
       book.read = req.body.read;
 
-      book.save();
+      req.book.save((err) => {
+        if (err) {
+          return res.send(err);
+        }
+        return res.json(book);
+      });
+    })
+    .patch((req, res) => {
+      const { book } = req;
 
-      return res.json(book);
+      if (req.body._id) {
+        delete req.body._id
+      }
+
+      Object.entries(req.body).forEach((item) => {
+        const key = item[0];
+        const value = item[1];
+
+        book[key] = value;
+      });
+
+      req.book.save((err) => {
+        if (err) {
+          return res.send(err);
+        }
+        return res.json(book);
+      });
+    })
+    .delete((req, res) => {
+      req.book.remove((err) => {
+        if (err) {
+          return res.send(err);
+        }
+        return res.sendStatus(204);
+      });
     });
 
   return bookRouter;
